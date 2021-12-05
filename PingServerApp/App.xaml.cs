4b6948d48d26585bf49a5ee61fd449037343d4c9
Shea4 <sc4gaming@gmail.com>
2021-12-05 15:23:54 +0000

    Add project files.

---
+++ PingServerApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PingServerApp {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        private void Application_Startup(object sender, StartupEventArgs e) {
+            Window window;
+            if (e.Args.Length == 1) {
+                window = new PingWindow(e.Args[0]);
+                if (((PingWindow) window).IsClosed) {
+                    return;
+                }
+            } else {
+                window = new MainWindow();
+            }
+
+            window.Show();
+        }
+
+    }
+}
---
+++ PingServerApp/PingWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.ComponentModel;
+
+namespace PingServerApp {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class PingWindow : Window {
+
+        private int Attempts = 0;
+        public bool IsClosed = false;
+        private CancellationTokenSource? TokenSource;
+
+        public PingWindow(string ip) {
+            InitializeComponent();
+            pingText.Text = "Failed to ping " + ip + " 0 times";
+            PingServer(ip);
+        }
+
+        public async void PingServer(string ip) {
+            this.TokenSource = new CancellationTokenSource();
+            CancellationToken token = TokenSource.Token;
+
+            Task task = Task.Run(() => {
+                Ping ping = new();
+                try {
+                    while (ping.Send(ip).Status != 0) {
+                        this.Dispatcher.Invoke(() => pingText.Text = "Failed to ping " + ip + " " + ++Attempts + " time" + (Attempts == 1 ? "" : "s"));
+
+                        Thread.Sleep(500);
+                    }
+                } catch (PingException) {
+                    MessageBox.Show("Invalid IP or name");
+                    return;
+                }
+
+                ping.Dispose();
+
+                MessageBox.Show(ip + " is online", "Status", MessageBoxButton.OK, MessageBoxImage.Information);
+            }, token);
+
+            await task;
+
+            this.Close();
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            this.IsClosed = true;
+            this.TokenSource.Cancel();
+            this.TokenSource.Dispose();
+        }
+
+    }
+}
